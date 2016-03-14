a067d320b41d175b33d6195af0b112b9e8f4eb17
Sharon Rumsey <sharon@rumsey.nz>
2016-03-14 21:35:05 +1300

    innerHTML not working

--- tictactoe.js
+++ tictactoe.js
@@ -7,21 +7,94 @@ var lines = [
 	[ 3, 6, 9],
 	[ 1, 5, 9],
 	[ 3, 5, 7]
-]
+];
 
+var msg;
+var msgElement;
 var board = [ '', 0, 0, 0, 0, -1, 0, 0, 0, 0 ];
 var cells = [];
+var amIStarted = 0
 
+// loading the page
+console.log('before');
+document.addEventListener("DOMContentLoaded", start);
 function start() {
-	var s 
+	console.log('load');
+	var s; 
 	for (var i = 1; i <= 9; i++) {
 		s = '#' + i.toString();
+		alert(s);
 		cells[i] = document.getElementById(s);
 	}
-}
+	msgElement = document.getElementById('msg');
+	displayBoard(board);
+	amIStarted = 1;
+};
+console.log('after');
 
+// displaying the board
 function displayBoard(board) {
 	for (var i = 1; i <= 9; i++) {
+		cells[i].innerHTML = board[i] < 0 ? "X" : board[i] > 0 ? "O" : " " ;
+	}
+	msgElement.innerHTML = msg;
+}
 
+function choose(cell) {
+	if (!amIStarted) {
+		start();
+	}
+	if (board[cell] != 0) {
+		msg = 'cell already taken';
+	} else {
+		board[cell] = 1;
+	}
+	move();
+}
+
+function move() {
+	// check if anyone has won the game
+	// checking for 3 in a row
+	var s;
+	for (var i = 0; i < lines.length; i++ ) {
+		s = board[lines[i][0]] + board[lines[i][1]] + board[lines[i][2]];
+		if (s == 3) {
+			msg = 'Player has won!';
+			displayBoard(board);
+			return;
+		} else if (s == -3) {
+			msg = 'Computer has won!';
+			displayBoard(board);
+			return;
+		}
+		// can computer win??
+		for (var i = 0; i < lines.length; i++) {
+			s = board[lines[i][0]] + board[lines[i][1]] + board[lines[i][2]];
+			if (s == -2) { // computer can win
+				for (var j = 0; j <= 2; j++) {
+					if (board[lines[i][j]] == 0) {
+						board[lines[i][j]] = -1;
+						displayBoard(board);
+						return;
+					}
+				}
+			}
+		}
+		// check can player win??
+		for (var i = 0; i < lines.length; i++) {
+			s = board[lines[i][0]] + board[lines[i][1]] + board[lines[i][2]];
+			if (s == 2) { // computer can win
+				for (var j = 0; j <= 2; j++) {
+					if (board[lines[i][j]] == 0) {
+						board[lines[i][j]] = 1;
+						displayBoard(board);
+						return;
+					}
+				}
+			}
+		}
 	}
 }
+
+
+
